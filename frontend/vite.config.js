6e2a8533f7ad2bc5a8046b5c63183f9416a1bd00
Your Name <your@email.com>
2026-05-10 20:58:28 +0500

    Fix Railway Vite host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    allowedHosts: [
+      'intuitive-adaptation-production-0108.up.railway.app'
+    ]
+  },
+
+  preview: {
+    allowedHosts: [
+      'intuitive-adaptation-production-0108.up.railway.app'
+    ]
+  }
 })
